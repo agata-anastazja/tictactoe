0a6e33a298293509a1d418de42613aeeac7d38d1
Agata Sumowska <agata.sumowska@gmail.com>
2017-01-26 12:49:20 +0000

    Adds make a move method that adds moves to the player's move array

--- lib/player.rb
+++ lib/player.rb
@@ -3,4 +3,8 @@ attr_reader :moves
   def initialize
     @moves = []
   end
+
+  def make_a_move(field)
+    @moves.push(field)
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,12 @@ require 'player'
 describe Player do
   subject(:player) {described_class.new}
 
-  it "stores the moves of a player" do
+  it "has an empty array to store moves of a player" do
     expect(player.moves).to eq []
+  end
+
+  it "adds moves with make a move method" do
+    player.make_a_move("A3")
+    expect(player.moves).to eq ["A3"]
   end
 end
